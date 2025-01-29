97cd7c6989a3748435310683344cb0ea69bce372
Italo1033 <italogammy1033@gmail.com>
2025-01-29 15:37:38 -0300

    atualizando Ihealthdatabase para pega mais de uma instancia

--- app/src/main/java/com/example/ihealth/database/IHealthDatabase.kt
+++ app/src/main/java/com/example/ihealth/database/IHealthDatabase.kt
@@ -1,24 +1,34 @@
 package com.example.ihealth.database
-
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.ihealth.database.dao.DrinkDao
+import com.example.ihealth.database.dao.UserDao
 import com.example.ihealth.database.entities.DrinkEntity
+import com.example.ihealth.database.entities.UserEntity
 
-@Database(entities = [DrinkEntity::class], version = 1)
-abstract class IHealthDatabase : RoomDatabase(){
+@Database(entities = [DrinkEntity::class, UserEntity::class], version = 2)
+abstract class IHealthDatabase : RoomDatabase() {
     abstract fun drinkDao(): DrinkDao
+    abstract fun userDao(): UserDao
 
     companion object {
+        @Volatile
+        private var INSTANCE: IHealthDatabase? = null
+
         fun getInstance(context: Context): IHealthDatabase {
-            return Room.databaseBuilder(
-                context,
-                IHealthDatabase::class.java,
-                "drinkHistory.db"
-            ).allowMainThreadQueries()
-                .build()
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    IHealthDatabase::class.java,
+                    "ihealth.db"
+                ).fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build()
+                INSTANCE = instance
+                instance
+            }
         }
     }
 }
