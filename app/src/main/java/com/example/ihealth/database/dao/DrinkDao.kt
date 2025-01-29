524cc21064424f1449a247f97dbf3031ccdd8300
Italo1033 <italogammy1033@gmail.com>
2025-01-29 15:38:06 -0300

    adicionando suspend ao drinkdao

--- app/src/main/java/com/example/ihealth/database/dao/DrinkDao.kt
+++ app/src/main/java/com/example/ihealth/database/dao/DrinkDao.kt
@@ -5,12 +5,11 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.ihealth.database.entities.DrinkEntity
-import com.example.ihealth.model.Drink
 
 @Dao
 interface DrinkDao {
     @Query("SELECT * FROM DrinkEntity")
-    fun findAll(): List<DrinkEntity>
+    suspend fun findAll(): List<DrinkEntity>
 
     @Insert
     fun save(drink: DrinkEntity)
